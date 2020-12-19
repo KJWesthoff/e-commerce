b4a0a0a7910b5abdaa8a7126b694ee4b75ec9367
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-12-19 01:31:02 -0800

    bug in tag-rutes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,41 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [{model:Category}]
+  }).then(dbRes => res.json(dbRes))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err); 
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      }
+    ],
+    where: {id: req.params.id}
+  }).then(dbRes => {
+    
+    if(!dbRes){    
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbRes)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err); 
+  });
+
+
+
 });
 
 // create new product
@@ -91,6 +120,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {id: req.params.id}
+  }).then(dbRes => {
+    if(!dbRes){
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+    res.json(dbRes);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
